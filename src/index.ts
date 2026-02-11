8b58f6112017b5085f9706897e7a6336c3ac718b
Lauri <lakoivisto@gmail.com>
2026-02-11 23:00:09 +0200

    add ship types

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,22 @@ import readline from 'readline'
 
 console.log("=== Let's play BATTLESHIP ===\n")
 
-type Cell = 'empty' | 'ship' | 'hit' | 'miss'
-
+type ShipType = 'patrol boat' | 'submarine' | 'destroyer'
+type Coordinate = { x: number; y: number }
+type Cell = 'empty' | 'hit' | 'miss' | number
 type Board = Cell[][]
+type ShipInfo = {
+  owner: 'player' | 'computer'
+  type: ShipType
+  length: number
+  cells: Coordinate[]
+  hitCount: number
+}
+type ShipRecord = Record<number, ShipInfo>
 
+// const ships: ShipInfo[] = []
 const BOARD_SIZE = 5
-// const SHIPS = [3, 2, 2]
+const ships: ShipRecord = {}
 
 function createBoard(size: number): Board {
   return Array.from({ length: size }, () => Array.from({ length: size }, () => 'empty'))
@@ -30,12 +40,12 @@ function printBoards(board: Board, hidden = false) {
         case 'hit':
           row += 'X '
           break
-        case 'ship':
-          if (hidden) row += '~ '
-          if (!hidden) row += '# '
+        case 'empty':
+          row += '~ '
           break
         default:
-          row += '~ '
+          if (hidden) row += '~ '
+          if (!hidden) row += `${y} `
       }
     }
     console.log(row)
@@ -72,7 +82,8 @@ async function playerTurn() {
     const coordinates = parseCoordinate(input)
 
     if (coordinates) {
-      switch (enemyBoard[coordinates[0]][coordinates[1]]) {
+      const cell = enemyBoard[coordinates[0]][coordinates[1]]
+      switch (cell) {
         case 'empty':
           enemyBoard[coordinates[0]][coordinates[1]] = 'miss'
           console.log('You miss!')
@@ -84,11 +95,12 @@ async function playerTurn() {
         case 'hit':
           console.log('Already shot here, try again')
           break
-        case 'ship':
-          enemyBoard[coordinates[0]][coordinates[1]] = 'hit'
-          console.log('You hit an enemy ship!')
+        default:
+          console.log(ships[cell])
+          ships[cell].cells
+          // enemyBoard[coordinates[0]][coordinates[1]] = 'hit'
+          // console.log('You hit an enemy ship!')
           validCoordinate = true
-          break
       }
     } else {
       console.log('Error parsing coordinates, please try again. Unknown coordinates:', input)
@@ -111,24 +123,75 @@ function computerTurn() {
         validCoordinate = true
         break
       case 'miss':
-        break
       case 'hit':
         break
     }
   }
 }
 
-function placeShip(board: Board) {
-  const x = Math.floor(Math.random() * BOARD_SIZE)
-  const y = Math.floor(Math.random() * BOARD_SIZE)
-
-  board[x][y] = 'ship'
+function placeShip(
+  board: Board,
+  shipName: ShipType,
+  shipSize: number,
+  shipId: number,
+  owner: 'player' | 'computer'
+) {
+  const ship: ShipInfo = {
+    owner: owner,
+    type: shipName,
+    length: shipSize,
+    cells: [],
+    hitCount: 0
+  }
+  let shipPlaced = false
+  let orientation = Math.floor(Math.random() * 2)
+  while (!shipPlaced) {
+    if (orientation === 0) {
+      const x = Math.floor(Math.random() * (BOARD_SIZE - shipSize + 1))
+      const y = Math.floor(Math.random() * BOARD_SIZE)
+      const shipArea = board
+        .map(row => row.slice(y, y + 1))
+        .flat()
+        .slice(x, x + shipSize)
+      if (shipArea.every(coordinate => coordinate === 'empty')) {
+        console.log(shipArea, orientation)
+        for (let i = x; i < x + shipSize; i++) {
+          board[i][y] = shipId
+          ship.cells.push({ x: i, y: y })
+          console.log(i, y, shipName)
+        }
+        ships[shipId] = ship
+        shipPlaced = true
+      }
+    } else {
+      const x = Math.floor(Math.random() * BOARD_SIZE)
+      const y = Math.floor(Math.random() * (BOARD_SIZE - shipSize + 1))
+      const shipArea = board[x].slice(y, y + shipSize)
+      if (shipArea.every(coordinate => coordinate === 'empty')) {
+        console.log(shipArea, orientation)
+        for (let i = y; i < y + shipSize; i++) {
+          board[x][i] = shipId
+          ship.cells.push({ x: x, y: i })
+          console.log(x, i, shipName)
+        }
+        ships[shipId] = ship
+        shipPlaced = true
+      }
+    }
+    orientation = Math.floor(Math.random() * 2)
+  }
 }
 
 const enemyBoard = createBoard(BOARD_SIZE)
 const playerBoard = createBoard(BOARD_SIZE)
-placeShip(enemyBoard)
-placeShip(playerBoard)
+
+placeShip(enemyBoard, 'destroyer', 3, 0, 'computer')
+placeShip(enemyBoard, 'destroyer', 3, 1, 'computer')
+placeShip(enemyBoard, 'patrol boat', 2, 2, 'computer')
+
+placeShip(playerBoard, 'destroyer', 3, 3, 'player')
+placeShip(playerBoard, 'destroyer', 3, 4, 'player')
+placeShip(playerBoard, 'patrol boat', 2, 5, 'player')
 
 function remainingShips(board: Board): boolean {
   for (const row of board) {
@@ -140,7 +203,7 @@ function remainingShips(board: Board): boolean {
 await enterInput('Press Enter to start!')
 
 console.log('\nEnemy Board\n')
-printBoards(enemyBoard, true)
+printBoards(enemyBoard)
 console.log('\nPlayer Board\n')
 printBoards(playerBoard)
 
